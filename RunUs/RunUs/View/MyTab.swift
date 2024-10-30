0de051aba3fc872b6bd5a90ba622e2f26222850b
slr-09 <ge12090@naver.com>
2024-10-30 20:55:34 +0900

    💄 Design: mypage tab user info view

--- RunUs/RunUs/View/MyTab.swift
+++ RunUs/RunUs/View/MyTab.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct MyTab: View {
     @Environment(\.dismiss) var dismiss
+    let userDefaults = UserDefaults.standard
     
     var body: some View {
+        ScrollView {
+            userInfo()
+                .padding(.vertical, 24)
+        }
+        .padding(.horizontal, 16)
         VStack {
             Button(action: {
                 UserDefaults.standard.removeObject(forKey: "idToken")
@@ -23,6 +29,35 @@ struct MyTab: View {
             })
             .background(.blue)
             .cornerRadius(10)
+        }
+    }
+    
+    @ViewBuilder
+    func userInfo() -> some View {
+        HStack(spacing: 24) {
+            // 프로필 사진
+            Image("default_user_profile")
+                .resizable()
+                .frame(width: 80, height: 80)
+            // 닉네임
+            VStack(alignment: .leading, spacing: 10) {
+                Text(userDefaults.string(forKey: "nickname") ?? "닉네임")
+                    .font(.title4_semibold)
+                    .foregroundStyle(.gray900)
+                Text("누적 거리: 12km")
+                Text("최근 러닝: 3일 전")
+            }
+            .font(.caption_regular)
+            .foregroundStyle(.gray500)
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image("edit_profile")
+            }
+
         }
     }
 }
