49ef530a215df872f4edda6573b0b9fc076457d5
slr-09 <ge12090@naver.com>
2024-11-04 11:36:28 +0900

    💄 Design: 경로선의 시작 점과 끝 점 원 그리기

--- RunUs/RunUs/View/Run/RunPath.swift
+++ RunUs/RunUs/View/Run/RunPath.swift
@@ -21,6 +21,9 @@ struct RunPath: View {
                 if let firstPoint = mapVM.userPath.first {
                     let startPoint = mapVM.coordinateToCGPoint(point: firstPoint, size: size)
                     path.move(to: startPoint)
+                    
+                    let circle = Path(ellipseIn: CGRect(origin: startPoint, size: CGSize(width: 22, height: 22)))
+                    context.fill(circle, with: .color(.primary500))
                 }
                 
                 // 이어 그리기
@@ -30,6 +33,15 @@ struct RunPath: View {
                 }
                 
                 context.stroke(path, with: .color(.primary500), lineWidth: 8)
+                
+                // 끝점
+                if let lastPoint = mapVM.userPath.last {
+                    let endPoint = mapVM.coordinateToCGPoint(point: lastPoint, size: size)
+                    path.move(to: endPoint)
+                    
+                    let circle = Path(ellipseIn: CGRect(origin: endPoint, size: CGSize(width: 22, height: 22)))
+                    context.fill(circle, with: .color(.primary500))
+                }
             }
         }
     }
