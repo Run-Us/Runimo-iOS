b33459b07bb1154ea141c9282ebde4cfe2f26028
jikbb2 <jikbb2@gmail.com>
2024-11-19 14:13:05 +0900

    🏷️ Add: add BaseResponse model

---
+++ RunUs/RunUs/Model/BaseResponse.swift
@@ -0,0 +1,15 @@
+//
+//  BaseResponse.swift
+//  RunUs
+//
+//  Created by byeoungjik on 11/19/24.
+//
+
+import Foundation
+
+class BaseResponse<T: Codable>: Codable {
+    let success: Bool
+    let message: String
+    let code: String
+    let payload: T?
+}
