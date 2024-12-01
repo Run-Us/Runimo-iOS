3c9d71d3b59e393f0cb31d17eb1857e5f5ad7517
slr-09 <ge12090@naver.com>
2024-12-01 16:04:38 +0900

    💄 Design: HomeTab - User Profile

--- RunUs/RunUs/View/Home/HomeTab.swift
+++ RunUs/RunUs/View/Home/HomeTab.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct HomeTab: View {
+    
     var body: some View {
-        Text("This is HomePage")
+        ScrollView {
+            ZStack {
+                Color.primaryBG
+                VStack {
+                    userProfile()
+                }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 24)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func userProfile() -> some View {
+        HStack(spacing: 12) {
+            VStack(alignment: .leading, spacing: 7) {
+                Text("\(UserDefaults.standard.string(forKey: "nickname") ?? "")님")
+                    .foregroundStyle(.primaryGray)
+                    .font(.title5_bold)
+                Text("Lv1 ∙ 12km")
+                    .foregroundStyle(.quaternaryGray)
+                    .font(.caption_semibold)
+                ProgressBar(progress: .constant(0.7))
+            }
+            Image("default_user_profile")
+                .resizable()
+                .frame(width: 60, height: 60)
+        }
     }
 }
 
