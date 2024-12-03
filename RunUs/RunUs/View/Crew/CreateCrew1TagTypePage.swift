8c1b6ceac108682e0200e1442fccb40be3a8ac50
slr-09 <ge12090@naver.com>
2024-12-03 18:19:28 +0900

    💄 Design: Create Crew 1 - Select Crew Type

--- RunUs/RunUs/View/Crew/CreateCrew1TagTypePage.swift
+++ RunUs/RunUs/View/Crew/CreateCrew1TagTypePage.swift
@@ -9,16 +9,40 @@ import SwiftUI
 
 struct CreateCrew1TagTypePage: View {
     @Environment(\.dismiss) var dismiss
+    @State private var selectedCrewIndex: Int = 0
+    private let crewTypeString: [[String]] = [
+        ["동네 친구", "내 동네와 비슷한 러너가 모여 같이 달려요."],
+        ["또래 친구", "비슷한 나이대의 러너들과 함께 공감하며 달려요."],
+        ["직장인", "일과 후 스트레스를 날려버릴 직장인 러너들과 함께해요."],
+        ["학생", "학교, 공부 얘기도 나누며 또래 학생 러너들과 달려요."],
+        ["MBTI", "성격이 잘 맞는 러너들과 함께 재밌게 달려봐요!"],
+        ["대회 준비", "목표가 같은 러너들과 함께 더 멀리, 더 빠르게 달려요."]
+    ]
     
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.primaryBG
-                VStack {
+                VStack(alignment: .leading) {
                     Divider()
-                    VStack {
-                        
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("어떤 성격의 크루를 만들고 싶은가요?")
+                            .font(.title5_bold)
+                            .foregroundStyle(.primaryGray)
+                        Text("크루의 테마를 지정해주세요")
+                            .font(.body2_medium)
+                            .foregroundStyle(.quaternaryGray)
+                        ForEach(Array(crewTypeString.enumerated()), id: \.offset) { index, crew in
+                            Button {
+                                selectedCrewIndex = index
+                            } label: {
+                                tagType(title: crew[0], explanation: crew[1], idx: index)
+                            }
+                            .buttonStyle(.plain)
+                        }
                     }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 24)
                     Spacer()
                 }
             }
@@ -35,6 +59,24 @@ struct CreateCrew1TagTypePage: View {
                 }
             }
         }
+    }
+    
+    @ViewBuilder
+    private func tagType(title: String, explanation: String, idx: Int) -> some View {
+        HStack(alignment: .top, spacing: 12) {
+            Image(idx == selectedCrewIndex ? "checkbox_selected" : "checkbox")
+                .resizable()
+                .frame(width: 20, height: 20)
+            VStack(alignment: .leading, spacing: 5) {
+                Text(title)
+                    .font(.body1_bold)
+                    .foregroundStyle(.secondaryGray)
+                Text(explanation)
+                    .font(.caption_regular)
+                    .foregroundStyle(.quaternaryGray)
+            }
+        }
+        .padding(.top, 32)
     }
 }
 
