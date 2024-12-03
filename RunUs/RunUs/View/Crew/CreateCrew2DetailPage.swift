8edda2874fec28b859b95ae7c3a6a123b84e316c
slr-09 <ge12090@naver.com>
2024-12-03 19:39:33 +0900

    💄 Design: CreateCrew2 - Crew Profile

--- RunUs/RunUs/View/Crew/CreateCrew2DetailPage.swift
+++ RunUs/RunUs/View/Crew/CreateCrew2DetailPage.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CreateCrew2DetailPage: View {
     @Environment(\.dismiss) var dismiss
+    @State private var selectedProfile: UIImage? = nil
     
     var body: some View {
         NavigationStack {
@@ -16,7 +17,22 @@ struct CreateCrew2DetailPage: View {
                 Color.primaryBG
                     .ignoresSafeArea()
                 ScrollView {
-
+                    Divider()
+                        .frame(height: 0.5)
+                        .background(.secondaryFill)
+                    VStack(spacing: 32) {
+                        VStack(spacing: 5) {
+                            Text("크루를 만들어볼까요?")
+                                .font(.title5_bold)
+                                .foregroundStyle(.primaryGray)
+                            Text("크루의 프로필을 등록해주세요")
+                                .font(.body2_medium)
+                                .foregroundStyle(.quaternaryGray)
+                        }
+                        .padding(.top, 24)
+                        
+                        profileImage()
+                    }
                 }
                 CTAButton(text: "다음", disabled: .constant(true)) {
                     
@@ -37,6 +53,23 @@ struct CreateCrew2DetailPage: View {
                 }
             }
         }
+    }
+    
+    @ViewBuilder
+    func profileImage() -> some View {
+        Button {
+        } label: {
+            Image(uiImage: selectedProfile ?? UIImage(named: "default_user_profile")!)   // TODO: change to crew default image
+                .resizable()
+                .scaledToFill()
+                .frame(width: 120, height: 120)
+                .clipShape(RoundedRectangle(cornerRadius: 52))
+        }
+        .overlay(alignment: .bottomTrailing) {
+            Image("plus_profile_button")
+        }
+        .padding(36)
+        
     }
 }
 
