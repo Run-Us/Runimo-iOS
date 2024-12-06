23f789603ebab577030014712d9d0beffa69ed03
slr-09 <ge12090@naver.com>
2024-12-06 19:22:40 +0900

    ✨ Feat: SessionCard 클릭 시 SessionDetailPage로 이동

---
+++ RunUs/RunUs/View/Session/SessionDetailPage.swift
@@ -0,0 +1,18 @@
+//
+//  SessionDetailPage.swift
+//  RunUs
+//
+//  Created by 가은 on 12/6/24.
+//
+
+import SwiftUI
+
+struct SessionDetailPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionDetailPage()
+}
