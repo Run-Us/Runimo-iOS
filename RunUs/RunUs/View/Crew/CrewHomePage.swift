21cd5d2b2a5c38c00bfc548820944afa50d21861
slr-09 <ge12090@naver.com>
2024-12-07 01:37:27 +0900

    💄 Design: CrewHomePage - Crew Profile

--- RunUs/RunUs/View/Crew/CrewHomePage.swift
+++ RunUs/RunUs/View/Crew/CrewHomePage.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct CrewHomePage: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.primaryBG.ignoresSafeArea()
+            ScrollView {
+                crewProfile()
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 20)
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .resizable()
+                            .frame(width: 8, height: 14)
+                            .foregroundStyle(.primaryGray)
+                    }
+                    .frame(width: 14)
+                    Text("Run with US 공식크루")
+                        .font(.body1_medium)
+                        .foregroundStyle(.primaryGray)
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    // TODO: Add Action
+                } label: {
+                    Image("vertical_dots")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.primaryGray)
+                        .frame(width: 20, height: 20)
+                }
+            }
+        }
+        .toolbarBackground(.primaryBG, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+    }
+    
+    @ViewBuilder
+    private func crewProfile() -> some View {
+        HStack(spacing: 24) {
+            Image("crew_default_profile")
+                .resizable()
+                .frame(width: 80, height: 80)
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Run US 공식크루")
+                    .font(.title4_semibold)
+                    .foregroundStyle(.primaryGray)
+                Text("Run with Us! 런어스의 공식크루 입니다.Run with Us! 런어스의 공식크루 입니다.")
+                    .lineLimit(1)
+                    .truncationMode(.tail)
+                Text("크루원 100명 · 동네친구")
+            }
+            .font(.body2_medium)
+            .foregroundStyle(.quaternaryGray)
+            Spacer()
+        }
     }
 }
 
