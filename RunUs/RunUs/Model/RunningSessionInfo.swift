c399002cde05d4700cadd661b1aecdc8dec4027d
slr-09 <ge12090@naver.com>
2024-12-06 15:51:53 +0900

    ✨ Feat: SessionCard creator data setting

--- RunUs/RunUs/Model/RunningSessionInfo.swift
+++ RunUs/RunUs/Model/RunningSessionInfo.swift
@@ -34,3 +34,26 @@ struct UserLocation: Codable {
     var latitude: Double
     var longitude: Double
 }
+
+struct SessionCardInfo: Codable {
+    let runing_public_id: String
+    let top_message: String
+    let title: String
+    let description: String
+    let start_at: String
+    let pace: [String]
+    let participant_count: Int
+    let created_by: SessionCreator
+    let crew: SessionCrew
+}
+
+struct SessionCreator: Codable {
+    let nickname: String
+    let profile_image: String?
+}
+
+struct SessionCrew: Codable {
+    let crew_public_id: String
+    let profile_image: String?
+    let name: String
+}
--- RunUs/RunUs/View/Session/SessionCard.swift
+++ RunUs/RunUs/View/Session/SessionCard.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct SessionCard: View {
+    let sessionCardData: SessionCardInfo
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Text("‘올공특공대’ 정기런")
+            Text(sessionCardData.top_message)
                 .font(.body2_medium)
                 .foregroundStyle(.primary400)
             Text("오후 4시 30분, 올림픽공원 평화의 문")
@@ -21,15 +22,7 @@ struct SessionCard: View {
             Text("페이스 5’30” - 6’00” - 빠른 걸음")
                 .font(.body1_medium)
                 .foregroundStyle(.gray400)
-            HStack(spacing: 8) {
-                Image("circle_profile")
-                    .resizable()
-                    .frame(width: 24, height: 24)
-                Text("생성한 사용자 이름")
-                Spacer()
-                Text("참가 인원: 2명")
-            }
-            .font(.caption_regular)
+            creator(isCreator: sessionCardData.top_message == "게스트 모집 중")
         }
         .foregroundStyle(.primaryGray)
         .padding(.horizontal, 16)
@@ -40,8 +33,40 @@ struct SessionCard: View {
                 .stroke(.secondaryFill, lineWidth: 1)
         )
     }
+    
+    @ViewBuilder
+    private func creator(isCreator: Bool) -> some View {
+        // top message에 따라 생성자 또는 생성 크루를 보여줌
+        let owner: (profileImage: String?, name: String) = isCreator ?
+        (sessionCardData.created_by.profile_image, sessionCardData.created_by.nickname) :
+        (sessionCardData.crew.profile_image, sessionCardData.crew.name)
+        
+        HStack(spacing: 8) {
+            if let profile = owner.profileImage {
+                AsyncImage(url: URL(string: profile))
+                    .clipShape(Circle())
+                    .frame(width: 24, height: 24)
+                    .overlay(
+                        Circle().stroke(.primary200, lineWidth: 1)
+                    )
+            } else {
+                Image(isCreator ? "default_user_profile" : "crew_default_profile")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(.primary200, lineWidth: 1)
+                    )
+            }
+            
+            Text(owner.name)
+            Spacer()
+            Text("참가 인원: \(sessionCardData.participant_count)명")
+        }
+        .font(.caption_regular)
+    }
 }
 
 #Preview {
-    SessionCard()
+    SessionCard(sessionCardData: SessionCardInfo(runing_public_id: "", top_message: "‘올공특공대’ 정기런", title: "", description: "", start_at: "", pace: [], participant_count: 1, created_by: SessionCreator(nickname: "생성자", profile_image: nil), crew: SessionCrew(crew_public_id: "", profile_image: nil, name: "크루명")))
 }
