19fbc6a70acca664106c5a5769ed43ad79123eff
slr-09 <ge12090@naver.com>
2024-12-16 15:07:20 +0900

    💄 Design: FindCrewPage toolbar

--- RunUs/RunUs/View/Crew/FindCrewPage.swift
+++ RunUs/RunUs/View/Crew/FindCrewPage.swift
@@ -23,6 +23,34 @@ struct FindCrewPage: View {
             .navigationDestination(isPresented: $showCreateCrewPage) {
                 CreateCrew1TagTypePage()
             }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack(spacing: 8) {
+                        Button {
+                            // TODO: Add Action
+                        } label: {
+                            Image(systemName: "chevron.left")
+                                .resizable()
+                                .frame(width: 8, height: 14)
+                                .foregroundStyle(.primaryGray)
+                        }
+                        .frame(width: 14)
+                        Text("크루 찾기")
+                            .font(.body1_medium)
+                            .foregroundStyle(.primaryGray)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        // TODO: Add Action
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: 14, height: 14)
+                            .foregroundStyle(.primaryGray)
+                    }
+                }
+            }
         }
     }
     
