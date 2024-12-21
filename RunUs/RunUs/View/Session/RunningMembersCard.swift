b2032a5f7e2982907c887be421860525e34a6cb1
slr-09 <ge12090@naver.com>
2024-12-21 17:17:28 +0900

    💄 Design: SessionDetailPage - Add MemberCard who participate in

---
+++ RunUs/RunUs/View/Session/RunningMembersCard.swift
@@ -0,0 +1,18 @@
+//
+//  RunningMembersCard.swift
+//  RunUs
+//
+//  Created by 가은 on 12/21/24.
+//
+
+import SwiftUI
+
+struct RunningMembersCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RunningMembersCard()
+}
