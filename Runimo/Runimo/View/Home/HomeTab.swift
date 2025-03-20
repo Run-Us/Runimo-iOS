87c5b9718db5110508df7e3f43e73d956dfc0409
slr-09 <ge12090@naver.com>
2025-03-20 21:33:46 +0900

    💄 Design: 홈 - 알 카드 추가

--- Runimo/Runimo/View/Home/HomeTab.swift
+++ Runimo/Runimo/View/Home/HomeTab.swift
@@ -13,6 +13,7 @@ struct HomeTab: View {
             Color.primaryBG
             VStack(spacing: 24) {
                 characterProfile()
+                eggCard()
             }
             .padding(.horizontal, 16)
             .padding(.vertical, 24)
@@ -28,7 +29,7 @@ struct HomeTab: View {
                     .foregroundStyle(.primaryGray)
                     .font(.title5_bold)
                 HStack(spacing: 40) {
-                    VStack(alignment: .leading) {
+                    VStack(alignment: .leading, spacing: 2) {
                         Text("러닝")
                             .font(.caption_regular)
                             .foregroundStyle(.quaternaryGray)
@@ -36,7 +37,7 @@ struct HomeTab: View {
                             .font(.title5_bold)
                             .foregroundStyle(.primaryGray)
                     }
-                    VStack(alignment: .leading) {
+                    VStack(alignment: .leading, spacing: 2) {
                         Text("달린 거리")
                             .font(.caption_regular)
                             .foregroundStyle(.quaternaryGray)
@@ -53,6 +54,54 @@ struct HomeTab: View {
         .background(
             RoundedRectangle(cornerRadius: 24)
                 .fill(.primaryFill)
+                .stroke(.secondaryFill, lineWidth: 1)
+        )
+    }
+    
+    @ViewBuilder
+    private func eggCard() -> some View {
+        VStack(spacing: 12) {
+            Image("home_egg_image")
+            HStack {
+                Text("마당 알")
+                    .font(.title5_bold)
+                    .foregroundStyle(.primaryGray)
+                Spacer()
+                Text("0/10")
+                    .font(.caption_regular)
+                    .foregroundStyle(.quaternaryGray)
+            }
+            .padding(.bottom, 12)
+            ProgressBar(progress: .constant(0.0))
+            giveLoveButton()
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 16)
+        .background(
+            RoundedRectangle(cornerRadius: 24)
+                .fill(.primaryFill)
+                .stroke(.secondaryFill, lineWidth: 1)
+        )
+    }
+    
+    @ViewBuilder
+    private func giveLoveButton() -> some View {
+        Button {
+            
+        } label: {
+            HStack(spacing: 8) {
+                Image("icon_love")
+                Text("애정 주기")
+                    .font(.title5_bold)
+                    .foregroundStyle(.primaryGray)
+            }
+            .padding(.vertical, 16)
+            .padding(.horizontal, 18)
+            .frame(maxWidth: .infinity)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 16)
+                .fill(.primaryBG)
                 .stroke(.secondaryFill, lineWidth: 1)
         )
     }
