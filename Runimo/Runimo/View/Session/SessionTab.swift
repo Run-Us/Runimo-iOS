aad3c36082b5752869552e626f0bbe5d9fe7b557
slr-09 <ge12090@naver.com>
2025-04-12 00:59:28 +0900

    🔥 Remove: 크루 & 세션 관련 코드 모두 삭제

--- Runimo/Runimo/View/Session/SessionTab.swift
+++ Runimo/Runimo/View/Session/SessionTab.swift
@@ -7,26 +7,6 @@
 
 import SwiftUI
 
-enum RunningPace: String {
-    case above500 = "ABOVE_500"
-    case pace500 = "500"
-    case pace530 = "530"
-    case pace600 = "600"
-    case pace630 = "630"
-    case pace700 = "700"
-    case pace730 = "730"
-    case pace800 = "800"
-    case fastWalk = "FAST_WALK"
-    
-    var description: String {
-        switch self {
-        case .above500: return "5’00” 이하"
-        case .fastWalk: return "빠른 걸음"
-        default: return "\(self.rawValue.dropLast(2))’\(self.rawValue.dropFirst())”"
-        }
-    }
-}
-
 struct SessionTab: View {
     @State private var selectedDateIndex: Int = 0
     @State private var showSessionDetailPage: Bool = false
