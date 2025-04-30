62d0adbb9e577ad8edc78575ea640fe0862a06a4
slr-09 <ge12090@naver.com>
2025-05-01 02:55:56 +0900

    💄 Design: 러닝 보상 뷰 추가

---
+++ Runimo/Runimo/View/Run/RunningRewardPage.swift
@@ -0,0 +1,65 @@
+//
+//  RunningRewardPage.swift
+//  Runimo
+//
+//  Created by 가은 on 5/1/25.
+//
+
+import SwiftUI
+
+struct RunningRewardPage: View {
+    @EnvironmentObject var navigation: NavigationManager
+    
+    var body: some View {
+        ZStack {
+            Color.primaryBG.ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 48) {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("오늘의 러닝을 성공적으로 마쳤어요!")
+                        .font(.title4_semibold)
+                        .foregroundStyle(.primaryGray)
+                    Text("nn분 동안 n.nnkm를 달렸어요. 정말 대단해요!")
+                        .font(.body2_medium)
+                        .foregroundStyle(.quaternaryGray)
+                }
+                .padding(.horizontal, 16)
+                
+                VStack(spacing: 12) {
+                    rewardItem(image: "icon_egg", text: "마당 알")
+                    rewardItem(image: "icon_love", text: "10개")
+                }
+                .padding(.horizontal, 16)
+                
+                Spacer()
+                
+                CTAButton(text: "다음", disabled: false) {
+                    navigation.path.append(FinishRunningPage.id)
+                }
+            }
+        }
+        .navigationBarBackButtonHidden()
+    }
+    
+    @ViewBuilder
+    private func rewardItem(image: String, text: String) -> some View {
+        HStack(spacing: 8) {
+            Spacer()
+            Image(image)
+            Text(text)
+                .font(.title5_bold)
+                .foregroundStyle(.primaryGray)
+                .padding(.vertical, 16)
+            Spacer()
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(.secondaryFill)
+        )
+    }
+}
+
+#Preview {
+    RunningRewardPage()
+}
