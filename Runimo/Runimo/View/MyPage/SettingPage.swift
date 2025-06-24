a9977d0351b78ac9eccb6b40d59c60ee1fcc6b17
slr-09 <ge12090@naver.com>
2025-06-24 17:19:56 +0900

    💄 Design: 설정 페이지 상단 네비 문구 변경
    "내 활동" -> "설정"

--- Runimo/Runimo/View/MyPage/SettingPage.swift
+++ Runimo/Runimo/View/MyPage/SettingPage.swift
@@ -60,12 +60,12 @@ struct SettingPage: View {
                         Image(systemName: "chevron.left")
                             .resizable()
                             .frame(width: 8, height: 14)
-                        Text("내 활동")
+                            .padding(.horizontal, 5)
+                        Text("설정")
                             .font(.body1_medium)
                     }
                     .foregroundStyle(.primaryGray)
                 }
-                .foregroundStyle(.primaryGray)
             }
         }
         .popup(isPresented: $showLogoutPopup, title: "로그아웃 하시겠어요?", subtitle: "로그아웃해도 러닝 활동은 삭제되지 않아요.", buttonText: "로그아웃 하기", buttonColor: .primary400) {
